320aa074ba08f93dc35ee85acfbccb5c97e3ab4e
Sunidhi Singh <sunidhisingh060402@gmail.com>
2026-05-02 12:18:42 +0530

    Add microphone input and speech recognition

--- voice/speech.py
+++ voice/speech.py
@@ -1,1 +1,15 @@
 
+import speech_recognition as sr
+
+def take_command():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        try:
+            audio = r.listen(source, timeout=5)
+            return r.recognize_google(audio, language="en-in").lower()
+        except:
+            return ""
