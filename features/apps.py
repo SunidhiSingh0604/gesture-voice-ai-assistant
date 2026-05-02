539117836a69fdbedfc97dbb5077829df2b8bcb7
Sunidhi Singh <sunidhisingh060402@gmail.com>
2026-05-02 12:21:48 +0530

    Add desktop app launcher via voice commands

--- features/apps.py
+++ features/apps.py
@@ -1,1 +1,12 @@
 
+import os
+from voice.tts import say
+
+def open_app(query):
+    if "notepad" in query:
+        os.system("notepad")
+        say("Opening Notepad")
+
+    elif "calculator" in query:
+        os.system("calc")
+        say("Opening Calculator")
