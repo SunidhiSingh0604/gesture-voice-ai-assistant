a50bc63f65cd458ddac6dcb4bc3798499ecd3fe9
Sunidhi Singh <sunidhisingh060402@gmail.com>
2026-05-02 12:21:16 +0530

    Add voice-controlled website navigation

--- features/browser.py
+++ features/browser.py
@@ -1,1 +1,12 @@
 
+import webbrowser
+from voice.tts import say
+
+def open_website(query):
+    if "youtube" in query:
+        webbrowser.open("https://youtube.com")
+        say("Opening YouTube")
+
+    elif "google" in query:
+        webbrowser.open("https://google.com")
+        say("Opening Google")
